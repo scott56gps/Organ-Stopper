8985cdd5e93276eee9f0d9329b8c1f61afeb56aa
Scott Nicholes <scott93gps@gmail.com>
2026-06-08 11:53:06 -0700

    Added advance and previous methods to CueDisplayView.  Added styling for buttons.

--- Organ Stopper/Views/CueDisplayView.swift
+++ Organ Stopper/Views/CueDisplayView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct CueDisplayView: View {
     let cues: [StopCueDetail]
-    let currentIndex: Int
     
+    @State var currentIndex: Int
     var currentCue: StopCueDetail { cues[currentIndex] }
 
     var body: some View {
@@ -28,7 +28,39 @@ struct CueDisplayView: View {
                     .listRowBackground(Color.blue.ignoresSafeArea([.all]))
                 }
             }
+            HStack(spacing: 0) {
+                Button("<") {
+                    regressCue()
+                }
+                .buttonStyle(RoundedButtonStyle())
+                Spacer()
+                Button(">") {
+                    advanceCue()
+                }
+                .buttonStyle(RoundedButtonStyle())
+            }
+            .padding()
         }
+    }
+    
+    private struct RoundedButtonStyle: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
+                .background(Color.accentColor.opacity(configuration.isPressed ? 0.7 : 1))
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .animation(.easeOut(duration: 0.1), value: configuration.isPressed)
+        }
+    }
+    
+    func advanceCue() {
+        currentIndex = (currentIndex < cues.count - 1) ? currentIndex + 1 : 0
+    }
+    func regressCue() {
+        currentIndex = currentIndex == 0 ? cues.count - 1 : currentIndex - 1
     }
 }
 
